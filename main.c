73143bdac10398a19872c0b7801381da84777c52
olewam <olewam1998@gmail.com>
2020-01-13 08:41:48 +0100

    And jupiter

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
-"include <stdio.h>
+#include <stdio.h>
 
 int main(){
 	printf("Hello world!\n");
-	printf("... and Mars\n");
+	printf("... and Jupiter\n");
 
 	if(1 < 0){
 		//In C, returning something non-zero signals an error
